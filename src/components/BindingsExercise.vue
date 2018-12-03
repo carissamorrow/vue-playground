b6b3cae81234627e289efdaf1fd284125b0c4fc9
carissa morrow <carissamorrow@hotmail.com>
2018-12-03 13:15:48 -0700

    half done

--- src/components/BindingsExercise.vue
+++ src/components/BindingsExercise.vue
@@ -9,8 +9,7 @@
                         the curlies.
                         Check out what happens below. Now go change the value of message in our components script area
                         and see what happens.</p>
-                    <p> {{
-                        <!-- Add your code inside the curlies --> }}</p>
+                    <p> {{"message"}}</p>
                 </div>
                 <div class="border p-1">
                     <h5>Two Way Data Binding</h5>
@@ -19,8 +18,7 @@
                         each time the value changes.</p>
                     <!-- add a v-model to this input below called "myName"-->
                     <input>
-                    <p> {{
-                        <!-- Add your code inside the curlies --> }}</p>
+                    <p> {{"myName"}}</p>
                 </div>
                 <div class="border p-1">
                     <h5>Class Binding</h5>
@@ -29,37 +27,28 @@
                         property(isActive) to true. Notice Vue will rerender each time the value changes.</p>
                     <div class="red">
                         <!-- add a class binding attribute to the p tag -->
-                        <p>This text will turn green on active</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-    </div>
-</template>
-
-
-<script>
-    export default {
-        name: "binding-exercises",
-        data() {
-            return {
-                //create a property called myName and set its value to "type your name here"
-                message: "Super secret message here!",
-                //change isActive to true 
-                isActive: false
-            }
-        }
-    }
-</script>
+                        <p :class="{active: isActive}" This text will turn green on active</p> </div> </div> </div> </div>
+                            </div> </template> <script>
+                                export default {
+                                    name: "myName",
+                                    data() {
+                                        return {
+                                            //create a property called myName and set its value to "type your name here"
+                                            message: "Super secret message here!",
+                                            //change isActive to true
+                                            isActive: true
+                                        }
+                                    }
+                                }
+                            </script>
 
 
-<style scoped>
-    .red {
-        color: red;
-    }
+                            <style scoped>
+                                .red {
+                                    color: red;
+                                }
 
-    .active {
-        color: green;
-    }
-</style>
+                                .active {
+                                    color: green;
+                                }
+                            </style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// @ts-ignore
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -15,6 +16,7 @@ export default new Router({
       path: '/start',
       name: 'start',
       component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "start" */ './views/Start.vue')
       }
     },
@@ -22,6 +24,7 @@ export default new Router({
       path: '/expressions',
       name: 'expressions',
       component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "expressions" */ './views/Expressions.vue')
       }
     },
@@ -29,6 +32,7 @@ export default new Router({
       path: '/bindings',
       name: 'bindings',
       component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "bindings" */ './views/Bindings.vue')
       }
     },
@@ -36,6 +40,7 @@ export default new Router({
       path: '/looping',
       name: 'looping',
       component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "looping" */ './views/Looping.vue')
       }
     },
@@ -43,6 +48,7 @@ export default new Router({
       path: '/computed-properties',
       name: 'computed-properties',
       component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "computed-properties" */ './views/ComputedProperties.vue')
       }
     },
@@ -50,6 +56,7 @@ export default new Router({
       path: '/conditional-rendering',
       name: 'conditional-rendering',
       component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "conditional-rendering" */ './views/ConditionalRendering.vue')
       }
     },
@@ -57,6 +64,7 @@ export default new Router({
       path: '/events',
       name: 'events',
       component: function () {
+        // @ts-ignore
         return import(/* webpackChunkName: "events" */ './views/Events.vue')
       }
     }
